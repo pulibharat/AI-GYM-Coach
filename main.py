5813fb909652f80e1c32f7f558d09343f57f82f8
puli bharat <pulibharat2007@gmail.com>
2026-06-20 19:26:29 +0530

    web rtc issue on streamlit

--- main.py
+++ main.py
@@ -262,8 +262,20 @@ def main():
             key="exercise-analysis",
             mode=WebRtcMode.SENDRECV,
             video_processor_factory=VideoProcessorClass,
-            rtc_configuration={"iceServers": [
-                {"urls": ["stun:stun.l.google.com:19302"]}]},
+            rtc_configuration={
+                "iceServers": [
+                    {"urls": ["stun:stun.l.google.com:19302"]},
+                    {
+                        "urls": [
+                            "turn:openrelay.metered.ca:80",
+                            "turn:openrelay.metered.ca:443",
+                            "turn:openrelay.metered.ca:443?transport=tcp"
+                        ],
+                        "username": "openrelayproject",
+                        "credential": "openrelayproject"
+                    }
+                ]
+            },
             media_stream_constraints={
                 "video": True,
                 "audio": False
